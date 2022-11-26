05b272fe4698b6fb84dee2d934b65512b986657e
romanchenko31 <romanchenko.zhen@gmail.com>
2022-11-26 19:13:14 +0200

    gallery

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,28 @@
-export const App = () => {
-  return <div></div>;
-};
+import React, { PureComponent } from 'react';
+import Searchbar from './Searchbar/Searchbar.jsx';
+import ImageGallery from './ImageGallery/ImageGallery.jsx';
+
+class App extends PureComponent {
+  state = {
+    searchQuery: null,
+  };
+
+  getValue = value => {
+    this.setState({
+      searchQuery: value,
+    });
+  };
+
+  render() {
+    const { searchQuery } = this.state;
+    return (
+      <>
+        <Searchbar onSubmit={this.getValue} />
+        {searchQuery && <ImageGallery searchQuery={searchQuery} />}
+        {!searchQuery && <h2>Найди фото</h2>}
+      </>
+    );
+  }
+}
+
+export { App };
